e4710521c93e88956ec98f90682e7d1ecef39971
Андрей Беляев <andrejbelaev@MacBook-Air-Andrej.local>
2023-06-03 17:29:18 +0500

    add auth + main projects module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { AuthModule } from './auth/auth.module'
+import { PrismaService } from './prisma.service'
+import { ConfigModule } from '@nestjs/config'
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [ConfigModule.forRoot(), AuthModule, UserModule],
+	controllers: [AppController],
+	providers: [AppService, PrismaService]
 })
 export class AppModule {}
